4527a69f0ec603a37e03a26dba97ba438295228e
JohnnyHsieh1020 <46452717+JohnnyHsieh1020@users.noreply.github.com>
2021-04-17 21:56:36 +0800

    js file

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+function delLog(logId){
+    fetch('/wlog/deleteLog', {
+        method: 'POST',
+        body: JSON.stringify({ logId:logId }),
+    }).then((_res) => {
+        // Reload the window with GET method
+        window.location.href = '/wlog/myWorkLog';
+    })
+}
