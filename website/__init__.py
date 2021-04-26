d5225fb3f0e8fac9ced637b4c57dc5c8b09bfd2b
JohnnyHsieh1020 <46452717+JohnnyHsieh1020@users.noreply.github.com>
2021-04-26 15:18:31 +0800

    update init

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Flask website123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/template1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user_name:password@IP:5432/db_name'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
